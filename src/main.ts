76e57d2a27c28278c71e7945d7332ff280cfe9b5
seungdeok <akk9132@naver.com>
2023-08-12 10:44:44 +0900

    feat: Configure dev server bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, VersioningType } from '@nestjs/common';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { PORT } from './config';
+
+const globalPrefix = 'api';
+const versionPrefix = 'v';
+const defaultVersion = '1';
+
+export const getApp = async () => {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.setGlobalPrefix(globalPrefix);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: versionPrefix,
+    defaultVersion: defaultVersion,
+  });
+
+  return app;
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await getApp();
+  await app.listen(PORT);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${PORT}/${globalPrefix}/${versionPrefix}${defaultVersion}`,
+  );
 }
-bootstrap();
+
+if (process.env.NODE_ENV === 'dev') {
+  bootstrap();
+}
